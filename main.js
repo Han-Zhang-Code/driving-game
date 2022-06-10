cca9fb160b69837a6ef24320579b1463324edeb8
Yuhan Zhang <zyh514312@gmail.com>
2022-06-10 03:14:24 +0000

    car stoped

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var x = 0;
 // var y = 0;
+var carStarted = false;
 var $car = document.querySelector('img');
+var intervalID = null;
 document.addEventListener('keydown', turnCar);
 function turnCar(event) {
   if (event.key === 'ArrowUp') {
@@ -12,14 +14,15 @@ function turnCar(event) {
   } else if (event.key === 'ArrowLeft') {
     $car.className = 'turn-left';
   }
-
-  if (event.key === ' ') {
-    //   var intervalID = setInterval(movingRight, 16);
-    movingRight();
+  if (event.key === ' ' && carStarted === false) {
+    intervalID = setInterval(movingRight, 16);
+    carStarted = true;
+  } else if (carStarted === true && event.key === ' ') {
+    clearInterval(intervalID);
+    carStarted = false;
   }
-
 }
 function movingRight() {
   x += 10;
-  $car.style.transform = 'rotate(0deg) translate(' + x + 'px)';
+  $car.style.transform = 'translate(' + x + 'px)';
 }
